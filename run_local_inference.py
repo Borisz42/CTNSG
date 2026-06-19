4a0396c2fe74ea6922c14cae2b2add0ad0331571
Borisz42 <borbely.david@gmail.com>
2026-06-20 00:26:09 +0200

    Add llm_int8_enable_fp32_cpu_offload to BitsAndBytes configs to prevent low-VRAM crashes

--- run_local_inference.py
+++ run_local_inference.py
@@ -26,7 +26,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 quantization_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_compute_dtype=torch.float16,
-    bnb_4bit_use_double_quant=True
+    bnb_4bit_use_double_quant=True,
+    llm_int8_enable_fp32_cpu_offload=True
 )
 base_model = AutoModelForCausalLM.from_pretrained(
     model_name,
